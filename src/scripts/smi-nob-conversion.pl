e3821de17b4266172a89b8c8634b5f642a75acde
Trond Trosterud <trond.trosterud@uit.no>
2019-09-20 20:44:48 +0000

    la til leksika frå smi-

--- src/scripts/smi-nob-conversion.pl
+++ src/scripts/smi-nob-conversion.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #
-# smi-sma-conversion.pl
-# Convert names in the SMI propernoun lexicon to SMA.
+# smi-smn-conversion.pl
+# Convert names in the SMI propernoun lexicon to smn.
 # $Id$
 
 use strict;
@@ -16,21 +16,79 @@ use open qw( :encoding(UTF-8) :std );
 
 while(<>) {
 	# Continuation lexicon substitutions:
-	s/ C-FI-NEN/nen LONDON/g ;
-	s/SUND/BERN/g ;
-	s/HEIM/BERN/g ;
-	s/SIJTE/ACCRA/g ;
-	s/HAWAII/ACCRA/g ;
 
-# Removing all sme special letters	
+
+
+	s/ C\-FI\-NEN/nen PROP/g ;
+	s/C\-FI\-NEN/nen PROP/g ;
+	s/ACCRA\-A_MWE/PROP/g ;
+	s/ACCRA\-A/PROP/g ;
+	s/ACCRA\-LOAN/PROP/g ;
+	s/ACCRA\-Y_MWE/PROP/g ;
+	s/ACCRA\-Y/PROP/g ;
+	s/ACCRA_MWE/PROP/g ;
+	s/ACCRA/PROP/g ;
+	s/BALAK/PROP/g ;
+	s/LONDONBERN/PROP/g ;
+	s/BERN\-LOAN/PROP/g ;
+	s/BERN_MWE/PROP/g ;
+	s/BERN/PROP/g ;
+	s/BETFAGE/PROP/g ;
+	s/BISSAU/PROP/g ;
+	s/CNAME_EVEN/PROP/g ;
+	s/DUBAI_MWE/PROP/g ;
+	s/DUBAI/PROP/g ;
+	s/DUORTNUS/PROP/g ;
+	s/ENDLEX/PROP/g ;
+	s/ERVASTI/PROP/g ;
+	s/GATA/PROP/g ;
+	s/GIRUNA/PROP/g ;
+	s/HANNOLA/PROP/g ;
+	s/HAWAII/PROP/g ;
+	s/HEIM/PROP/g ;
+	s/JOKULL/PROP/g ;
+	s/LONDON\-LOAN/PROP/g ;
+	s/LONDON_MWE/PROP/g ;
+	s/LONDON/PROP/g ;
+	s/MACKENZIE/PROP/g ;
+	s/MAKI/PROP/g ;
+	s/MALAWI/PROP/g ;
+	s/MOLDAVIA\-LOAN/PROP/g ;
+	s/MOLDAVIA/PROP/g ;
+	s/NIEJTE/PROP/g ;
+	s/NIEMI/PROP/g ;
+	s/NIKOSIIJA/PROP/g ;
+	s/NYSTØ\-LOAN/PROP/g ;
+	s/NYSTØ_MWE/PROP/g ;
+	s/NYSTØ/PROP/g ;
+	s/OY/PROP/g ;
+	s/PERU/PROP/g ;
+	s/PIPPI\-LOAN/PROP/g ;
+	s/PIPPI_MWE/PROP/g ;
+	s/PIPPI/PROP/g ;
+	s/RONDANE\-SG\-LOAN/PROP/g ;
+	s/RONDANE\-SG/PROP/g ;
+	s/RONDANE\-LOAN/PROP/g ;
+	s/RONDANE/PROP/g ;
+	s/SIJTE/PROP/g ;
+	s/SKANIK/PROP/g ;
+	s/SUND\-LOAN/PROP/g ;
+	s/SUND/PROP/g ;
+	s/\+CmpNP\/None//g ;
+	s/flagK/K/g ;
+	
+
+# sme special symbols
 	s/b9/b/g ;
+	s/e9/e/g ;
 	s/d9/d/g ;
 	s/g9/g/g ;
 	s/h9/h/g ;
 	s/j9/j/g ;
 	s/k9/k/g ;
-	s/m9/m/g ;
+	s/m9/m5/g ;
 	s/n9/n/g ;
+	s/o9/o/g ;
 	s/p9/p/g ;
 	s/r9/r/g ;
 	s/s9/s/g ;
@@ -40,31 +98,9 @@ while(<>) {
 	s/7 / /g ;
 	s/8 / /g ;
 	s/9 / /g ;
-	s/7#/#/g ;
-	s/8#/#/g ;
-	s/9#/#/g ;
-
-	# Substitutions due to orthographic differences between SMJ and SME:
-	s/t:(.*)h /t:$1d9 /g ;
-	s/čč/tsj/g ;
-	s/Č/Tsj/g ;
-	# Andreevič -> Andreevitj:
-#	s/^(.*)č /$1tsj:$1tsj /g ;
-	s/č/tsj/g ;
-	s/šž/sjtsj/g ;
-	s/Šž/Sjtsj/g ;
-	s/šš/ssj/g ;
-	s/žž/dtsj/g ;
-	s/Š/Sj/g ;
-	s/š/sj/g ;
-	s/ž/dsj/g ;
-	# j->i || Vow i .* : .* Vow _ ; Heaika:Heajka -> Heaika:Heaika. But Majken 	
-#	s/([ÁAEIOUaáeiou])i(.*):(.*)([ÁAEIOUaáeiou])j/$1i$2:$3$4i/g ;
-    s/([ÁAEIOUaáeiou])i([^j].*):(.*)([ÁAEIOUaáeiou])j([^ÁAEIOUaáeiou])/$1i$2:$3$4i$5/g ;
 
 
 	my $line = $_;
 
 	print $line;
 }
-
