cc723959c3bf4a56658837b1f15ee17e2ff76ac2
leneantonsen <lene.antonsen@uit.no>
2020-11-02 19:03:56 +0100

    forbedring, mer robust

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -3,8 +3,8 @@
 echo 'Are there tags not declared in root.lexc or misspelled?'
 cat src/fst/compounding.lexc \
     src/fst/affixes/*lexc \
-    src/fst/stems/*lexc  ../../giella-shared/smi/src/fst/stems/*lexc | \
-    cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+    src/fst/stems/*lexc  ../giella-shared/smi/src/fst/stems/*lexc | \
+    cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
 cat src/fst/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
